b433578c2ec7807e63dc9135471d74e278ca0195
Muhammad <mdzhanov@gmail.com>
2024-02-07 23:13:29 +0100

    check

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import Board from "widgets/Board/Board";
 const Home = () => {
   return (
     <div className="flex w-full h-[100dvh] justify-center items-center">
+      check
       <Board />
     </div>
   );
